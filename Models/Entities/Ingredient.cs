b0a261277359155c6ee83076e47c460262c5eb6c
Dienes Viktor <dienesviktor@gmail.com>
2022-08-01 16:34:51 +0200

    create recipe and ingredient class, and set a dbset for them

---
+++ Models/Entities/Ingredient.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HogwartsPotions.Models.Entities
+{
+    public class Ingredient
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long ID { get; set; }
+
+        public int Name { get; set; }
+    }
+}
---
+++ Models/Entities/Recipe.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HogwartsPotions.Models.Entities
+{
+    public class Recipe
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long ID { get; set; }
+
+        public int Name { get; set; }
+
+        public Student Student { get; set; }
+    }
+}
